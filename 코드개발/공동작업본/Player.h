be18ecd93ac3afed964885a0f3423f08176b4df8
Chaeso99 <71647858+Chaeso99@users.noreply.github.com>
2020-11-27 22:23:02 +0900

    Update Player.h

--- 코드개발/공동작업본/Player.h
+++ 코드개발/공동작업본/Player.h
@@ -1,6 +1,7 @@
-#pragma once
-#include <iostream>
+#ifndef PLAYER_HEADER
+#define PLAYER_HEADER
 
+#include <iostream>
 using namespace std;
 
 class Player
@@ -19,10 +20,21 @@ public:
 	{
 
 	}
+
+	//ioput function
+	int getHealth() { return Health; }
+	int getArk() { return Ark; }
+	int getDef() { return Def; }
+
+	void setHealth(int Health) { this->Health = Health; }
+	void setArk(int Ark) { this->Ark = Ark; }
+	void setDef(int Def) { this->Def = Def; }
 	
-	void hi()
+	void hi() //print player info
 	{
 		cout << "플레이어(체력:" << Health << " 공격력:" << Ark << " 방어력:" << Def << ") 생성" << endl;
 	}
 	
 };
+
+#endif
